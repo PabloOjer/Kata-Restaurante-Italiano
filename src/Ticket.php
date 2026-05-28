73cf15407a89e5a85257b9904cb234fb0e1716e9
PabloOjer <ojer.161782@e.unavarra.es>
2026-05-28 16:55:34 +0200

    [*] - Añadido red test de funcion añadir plato con valores

--- src/Ticket.php
+++ src/Ticket.php
@@ -4,14 +4,14 @@ namespace Deg540\CleanCodeKata9;
 
 class Ticket
 { 
-    int $sumaPrecio = 0;
-    $platos = array();
+    private int $sumaPrecio = 0;
+    private $platos = array();
     
     public function __construct()
     {
     }
 
-    public function añadir ($platos, int $precio): bool
+    public function añadir (string $platos, int $precio): bool
     {
         return true;
     }
