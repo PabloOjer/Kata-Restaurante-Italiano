317f630555e85432016572e65a40b93e825135a0
PabloOjer <ojer.161782@e.unavarra.es>
2026-05-28 17:05:06 +0200

    [*] - Implementar getPrice en funcion añadir.

--- src/Ticket.php
+++ src/Ticket.php
@@ -7,15 +7,16 @@ use Deg540\CleanCodeKata9\Menu;
 class Ticket
 { 
     private int $sumaPrecio = 0;
-    private $platos = array();
     
     public function __construct()
     {
+        private $menu = new Menu();
+
     }
 
-    public function añadir (string $platos, int $precio): bool
+    public function añadir (string $platos): bool
     {
-        return true;
+        return($this->menu->getPrice($platos));
     }
 }
 ?>
