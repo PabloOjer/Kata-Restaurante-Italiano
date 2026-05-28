a2408b4645bdfe9cb235e85ba19a4dc07ad25b86
PabloOjer <ojer.161782@e.unavarra.es>
2026-05-28 16:49:37 +0200

    [*] - Refactor funcion añadir plato.

--- src/Ticket.php
+++ src/Ticket.php
@@ -4,11 +4,14 @@ namespace Deg540\CleanCodeKata9;
 
 class Ticket
 { 
+    int $sumaPrecio = 0;
+    $platos = array();
+    
     public function __construct()
     {
     }
 
-    public function añadir (): bool
+    public function añadir ($platos, int $precio): bool
     {
         return true;
     }
