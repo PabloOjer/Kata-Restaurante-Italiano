041a4b46fc2d3ec0ec0f7ecface48f4a0ed1e733
PabloOjer <ojer.161782@e.unavarra.es>
2026-05-28 17:47:17 +0200

    [*] - Añadir test añadir varios platos iguales.

--- tests/PublicTest.php
+++ tests/PublicTest.php
@@ -36,5 +36,15 @@ final class PublicTest extends TestCase
         $result = $ticket->añadir("hamburguesa");
         $this->assertEquals("El plato seleccionado no existe en el menú",$result);
     }
+    /**
+     * @test
+     */
+    public function testAñadirVariosPlatos(): void
+    {
+        $ticket = new Ticket();
+        $ticket->añadir("pizza");
+        $ticket->añadir("pasta");
+        $this->assertEquals(18, $ticket->sumaPrecio);
+    }   
 }
 ?>
